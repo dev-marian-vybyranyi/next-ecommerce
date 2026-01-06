4b7d5f74388080896370b0591dbaa0b13f33cab7
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 14:18:19 +0200

    Add user seeding with hashed passwords and update seed script dependencies.

--- db/seed.ts
+++ db/seed.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 import sampleData from "./sample-data";
+import "dotenv/config";
 
 async function main() {
   const connectionString = `${process.env.DATABASE_URL}`;
@@ -10,8 +11,13 @@ async function main() {
   const adapter = new PrismaPg(pool);
   const prisma = new PrismaClient({ adapter });
   await prisma.product.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
+  await prisma.user.createMany({ data: sampleData.users });
 
   console.log("Database seeded successfully!");
 }
