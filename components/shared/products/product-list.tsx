91b36eef49cd92ce55bb3fe6d518edad28ac90b4
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 11:27:56 +0200

    Add Card UI component and refactor ProductList to display products using ProductCard.

--- components/shared/products/product-list.tsx
+++ components/shared/products/product-list.tsx
@@ -1,22 +1,16 @@
-const ProductList = ({
-  data,
-  title,
-  limit,
-}: {
-  data: any;
-  title?: string;
-  limit?: number;
-}) => {
-  const limitedData = limit ? data.slice(0, limit) : data;
+import ProductCard from "./product-card";
 
+const ProductList = ({ data, title }: { data: any; title?: string }) => {
   return (
     <div className="my-10">
-      <h2 className="h2-bold nb-4">{title}</h2>
+      <h2 className="h2-bold mb-4">{title}</h2>
       {data.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (
-            <div key={product.id}>{product.name}</div>
-          ))}
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {data.map((product: any) => (
+              <ProductCard product={product} key={product.slug} />
+            ))}
+          </div>
         </div>
       ) : (
         <div>
