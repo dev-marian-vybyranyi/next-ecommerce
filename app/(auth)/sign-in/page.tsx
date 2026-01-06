cd575abdc62a35c34344e7d68f40039c4d74e23f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 14:49:46 +0200

    implement credentials sign-in form using new `Label` and `Input` UI components.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -5,10 +5,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Metadata } from "next";
-import Link from "next/link";
-import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import CredentialsSignInForm from "./credentials-signin-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -33,7 +34,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* FORM HERE */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
