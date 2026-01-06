a82de9abe87abce976a28ecb3d106c9ba38f0120
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 16:48:23 +0200

    Implement NextAuth middleware for session cart ID management and pre-fill sign-in defaults.

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,10 +4,9 @@ export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhos
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: '',
-  password: '',
+  email: 'admin@example.com',
+  password: '123456',
 };
-
 
 export const signUpDefaultValues = {
   name: '',
