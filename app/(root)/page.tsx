48faa39905d9178f25a992167870653f311dffbc
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 11:22:55 +0200

    add ProductList component and display sample product data on the homepage.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,10 @@
+import ProductList from "@/components/shared/products/product-list";
+import sampleData from "@/db/sample-data";
+
 export default function Home() {
-  return <div>page</div>;
+  return (
+    <div>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+    </div>
+  );
 }
