fc8ff1b68c4c888cf0eda3b68aa95cae195e515a
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 11:10:36 +0200

    Add responsive header menu component utilizing a new sheet UI.

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { ShoppingCart, UserIcon } from "lucide-react";
+import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -14,30 +12,16 @@ const Header = () => {
             <Image
               src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
-              width={48}
               height={48}
-              priority
+              width={48}
+              priority={true}
             />
             <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
