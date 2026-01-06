32d4259a915349ee558f70c51383509c40a54e95
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-06 15:31:02 +0200

    implement user sign-up functionality with form validation, password hashing, and user creation.

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,9 @@
 "use server";
-import { signInFormSchema } from "../validators";
+
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
-import { redirect } from "next/navigation";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -13,13 +15,11 @@ export async function signInWithCredentials(
       email: formData.get("email"),
       password: formData.get("password"),
     });
+
     await signIn("credentials", user);
+
     return { success: true, message: "Signed in successfully" };
   } catch (error) {
-    // Перевіряємо чи це помилка редиректу
-    if (error instanceof Error && error.message === "NEXT_REDIRECT") {
-      throw error;
-    }
     return { success: false, message: "Invalid email or password" };
   }
 }
@@ -27,4 +27,37 @@ export async function signInWithCredentials(
 // Sign user out
 export async function signOutUser() {
   await signOut();
+}
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    return { success: false, message: "User was not registered" };
+  }
 }
