4d0892f470f5af5374eae8773ba11f5a542e679f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-07 09:58:15 +0200

    Implement shipping address form using `react-hook-form` and `Zod`, introducing a new generic form UI.

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -1,28 +1,33 @@
-import { auth } from '@/auth';
-import { getMyCart } from '@/lib/actions/cart.actions';
-import { getUserById } from '@/lib/actions/user.actions';
-import { Metadata } from 'next';
-import { redirect } from 'next/navigation';
-import { ShippingAddress } from '@/types';
+import { auth } from "@/auth";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { getUserById } from "@/lib/actions/user.actions";
+import { ShippingAddress } from "@/types";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
-  title: 'Shipping Address',
+  title: "Shipping Address",
 };
 
 const ShippingAddressPage = async () => {
   const cart = await getMyCart();
 
-  if (!cart || cart.items.length === 0) redirect('/cart');
+  if (!cart || cart.items.length === 0) redirect("/cart");
 
   const session = await auth();
 
   const userId = session?.user?.id;
 
-  if (!userId) throw new Error('No user ID');
+  if (!userId) throw new Error("No user ID");
 
   const user = await getUserById(userId);
 
-  return <>Address</>;
+  return (
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
