75ea09f9816e4699d5b79f81f81ad33909d8d94e
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-07 15:21:46 +0200

    Implement user profile editing with session updates, refine order pagination, and adjust default page size.

--- auth.ts
+++ auth.ts
@@ -106,6 +106,12 @@ export const config = {
           }
         }
       }
+
+      // Handle session updates
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+
       return token;
     },
   },
