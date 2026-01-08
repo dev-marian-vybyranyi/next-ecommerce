80482ecc1522d51a24f3595e4f3a5efdd0d95ba9
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-08 16:06:10 +0200

    add posibility to update rewiev

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { getMyCart } from "@/lib/actions/cart.actions";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 import ReviewList from "./review-list";
+import Rating from "@/components/shared/products/rating";
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -37,9 +38,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand} {product.category}
               </p>
               <h1 className="h3-bold">{product.name}</h1>
-              <p>
-                {product.rating} of {product.numReviews} Reviews
-              </p>
+              <Rating value={Number(product.rating)} />
+              <p>{product.numReviews} reviews</p>
               <div className="flex flex-col sm:flex-row sm:items-center gap-3">
                 <ProductPrice
                   value={Number(product.price)}
@@ -91,7 +91,7 @@ const ProductDetailsPage = async (props: {
         </div>
       </section>
       <section className="mt-10">
-        <h2 className="h2-bold">Customer Reviews</h2>
+        <h2 className="h2-bold mb-5">Customer Reviews</h2>
         <ReviewList
           userId={userId || ""}
           productId={product.id}
